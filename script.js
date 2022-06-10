1553f3ccda74ad884d1ea3b2311244fceca1bcfd
Sujit Akulwar <sujitakulwar@gmail.com>
2022-06-10 13:57:12 -0700

    change 2

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
-function clearScreen() {
-    document.getElementById("result").value = "";
+function calculate() {
+    var p;
+    var q;
+    p = document.getElementById("result").value;
+    q = eval(p);
+    document.getElementById("result").value = q;
 }
-   
+
 function display(value) {
     document.getElementById("result").value += value;
 }
 
-function calculate() {
-    var p = document.getElementById("result").value;
-    var q = eval(p);
-    document.getElementById("result").value = q;
+function clearScreen() {
+    document.getElementById("result").value = "";
 }
+   
+
+
